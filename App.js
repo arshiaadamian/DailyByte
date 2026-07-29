215f09e54ef8aa77396d04afe801979f7dadfd8f
Arshia <arshia@Arshias-MacBook-Pro.local>
2026-07-28 19:50:19 -0700

    changes to main

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is Daily Byte</Text>
+      <Text>This is Daily Byte1</Text>
       <StatusBar style="auto" />
     </View>
   );
